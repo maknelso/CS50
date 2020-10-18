9424d77b0c0363aafa0fe00299e665c620c7af8e
maknelso <55319541+maknelso@users.noreply.github.com>
2020-10-18 16:14:58 -0700

    Update PS7_roster.py

--- PS7_roster.py
+++ PS7_roster.py
@@ -1,8 +1,5 @@
 # Program should accept the name of a house as a CLA
-
 # e.g. $ python roster.py Gryffindor
-# Hermione Jean Granger, born 1979
-
 from sys import argv, exit
 import cs50
 from cs50 import SQL
@@ -12,7 +9,15 @@ if len(argv) != 2:
     print("missing command-line argument: please include house")
     exit(1)
 
+# Open the file for SQLite
 db = cs50.SQL("sqlite:///students.db")
 
-for row in db.execute("SELECT * FROM Students LIMIT 5;"):
-    print(row)
+rows = db.execute("SELECT * FROM Students WHERE house = ? ORDER BY lName, fName", argv[1])
+for row in rows:
+    # Give myself alias for easier typing
+    fName, mName, lName, birth = row["fName"], row["mName"], row["lName"], row["birth"]
+
+    if mName == None:
+        print(f"{fName} {lName}, born {birth}")
+    elif mName != None:
+        print(f"{fName} {mName} {lName}, born {birth}")
