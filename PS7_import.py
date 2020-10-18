fec8b715b61dbc05148e293aa905f77ea94ddb3e
maknelso <55319541+maknelso@users.noreply.github.com>
2020-10-17 17:02:28 -0700

    Update PS7_import.py

--- PS7_import.py
+++ PS7_import.py
@@ -1,6 +1,5 @@
-#WIP
-
 # e.g. $ python import.py characters.csv
+# after running above - run sqlite3 students.db to run SQL
 # Take CSV and import that data into a SQLite db
 from sys import argv, exit
 import cs50
@@ -19,7 +18,7 @@ db = cs50.SQL("sqlite:///students.db")
 
 # Create table called students in database file called students.db
 # Specific columns we want - db.execute("CREATE TABLE students(fName TEXT, mName TEXT, lName TEXT, house TEXT, year TEXT")
-db.execute("CREATE TABLE students (name TEXT, house TEXT, birth TEXT)")
+db.execute("CREATE TABLE students (fname TEXT, mName TEXT, lName TEXT, house TEXT, birth TEXT)")
 
 # Open CSV file
 with open(argv[1], "r") as characters:
@@ -31,5 +30,15 @@ with open(argv[1], "r") as characters:
         house = row["house"]
         birth = row["birth"]
 
-        # Insert into students db
-        db.execute("INSERT INTO students(name, house, birth) VALUES(?, ?, ?)", name, house, birth)
+        # Use split function on name to split into words
+        # example 1: Hannah Abbott      => Hannah, Abbott
+        # example 2: Harry James Potter => Harry, James, Potter
+        splitName = name.split(" ")
+
+        # full name does not have a middle name
+        if len(splitName) == 2:
+            db.execute("INSERT INTO students(fName, mName, lName, house, birth) VALUES(?, ?, ?, ?, ?)", splitName[0], None, splitName[1], house, birth)
+
+        # full name has a middle name
+        elif len(splitName) == 3:
+            db.execute("INSERT INTO students(fName, mName, lName, house, birth) VALUES(?, ?, ?, ?, ?)", splitName[0], splitName[1], splitName[2], house, birth)
